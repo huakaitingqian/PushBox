321ff7093b513e0314119827a13fc8ca226265cf
静灵庭 <huakaitingqian@hotmail.com>
2015-08-03 20:09:34 +0800

    start

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Data : MonoBehaviour {
+
+	// Use this for initialization
+    static int mapWidth = 15;
+    static int mapHeight = 15;
+	
+    public static int GetMapWidth()
+    {
+        return mapWidth;
+    }
+
+    public static int GetMapHeight()
+    {
+        return mapHeight;
+    }
+
+    public static void SetMapWidth(int w) 
+    {
+        mapWidth = w;
+    }
+
+    public static void SetMapHeight(int h)
+    {
+        mapHeight = h;
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        Debug.Log(this.gameObject.name);
 	}
 	
 	// Update is called once per frame
